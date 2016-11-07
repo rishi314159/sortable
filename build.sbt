76f90e7630f2f2b1f62a06ebc6a8585a78c91d35
Rishikesh <rishi314159@gmail.com>
2016-11-07 15:13:57 -0500

    first commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+scalaVersion := "2.11.8"
+
+
+
+
+
+libraryDependencies ++= Seq(
+    "com.typesafe.play" %% "play-json" % "2.5.8",
+    "org.json4s" %% "json4s-native" % "3.5.0")
+
+
---
+++ src/main/scala/sortable/Products.scala
@@ -0,0 +1,27 @@
+package sortable
+import play.api.libs.json._
+
+class Products(val filename:String){
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  private def constructProductTable(filename: String) {
+    
+  }
+}
