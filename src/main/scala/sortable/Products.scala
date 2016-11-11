44746ca80a34ee63b1961216420d3020daa2fc88
Rishikesh <rishi314159@gmail.com>
2016-11-11 09:46:14 -0500

    modified find to reduce false positive

--- src/main/scala/sortable/Products.scala
+++ src/main/scala/sortable/Products.scala
@@ -17,7 +17,7 @@ class Products(val filename:String) extends ProductsTrait{
   def find(listing:Listing):Option[Product] = {
     
     val manufacturer = listing.value("manufacturer").toString().toLowerCase().replaceAll("[^A-Za-z0-9]","")
-    val title = listing.value("title").toString().toLowerCase().replaceAll("[^A-Za-z0-9]","")
+    val title = listing.value("title").toString().toLowerCase().replaceAll("[^A-Za-z0-9 ]","")
     
     val foundManufacturer = manufacturers.find { x => manufacturer.contains(x)}
     
@@ -25,14 +25,29 @@ class Products(val filename:String) extends ProductsTrait{
     foundManufacturer match {
       case None => None
       case Some(manuf) => {
-        val models = dataMap(manuf).keySet
-        val foundModel = models.find ( x => title.contains(x))
-        foundModel match {
+        val productnames = dataMap(manuf).keySet
+        val foundProduct = productnames.find ( x => matchAll(x,title))
+        foundProduct match {
           case None => None
-          case Some(m) => Some(dataMap(manuf)(m))
+          case Some(m) => {
+            val pd = dataMap(manuf)(m)
+            val title2 = title.replaceAll("[^A-Za-z]", "")
+            val model = pd.value("model").toString().toLowerCase().replaceAll("[^a-z]", "")
+            if (title2.contains(model))
+              Some(pd)
+              else
+                None
+
+          }
         }
       }
     }
+
+  }
+  
+  def matchAll(productname:String ,title:String):Boolean = {
+    val splitProductname = productname.split(" ")
+    splitProductname.map(x => title.contains(x)).foldLeft(true)( _ && _)
 
   }
   
@@ -58,7 +73,7 @@ class Products(val filename:String) extends ProductsTrait{
   private def extendMap(initial:Map[String, Map[String, Product]], itemValue: JsValue) = {
     val item = itemValue.as[JsObject]
       val manufacturer =  item.value("manufacturer").toString().toLowerCase().replaceAll("[^A-Za-z0-9]","")
-      val model =  item.value("model").toString().toLowerCase().replaceAll("[^A-Za-z0-9]","")
+      val model =  item.value("product_name").toString().toLowerCase().replaceAll("[^a-z0-9 ]"," ")
       initial.updated(manufacturer, initial(manufacturer).updated(model, item))
   }
 }
